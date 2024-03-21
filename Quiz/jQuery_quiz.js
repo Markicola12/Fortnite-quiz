32be9c92a19cd0331b62d30e854511f1119857e0
Markicola12 <antalm481@gmail.com>
2024-03-21 21:52:38 +0100

    FQ-15 értékelést kiválasztó felület

---
+++ Quiz/jQuery_quiz.js
@@ -0,0 +1,76 @@
+$(document).ready(
+    function(){
+        $(".nagyon_nem").click(
+            function(event)
+        {
+            $(this).addClass("kivalasztva").siblings().removeClass("kivalasztva");
+        }
+        );
+    }
+);
+
+$(document).ready(
+    function(){
+        $(".félig_nem").click(
+            function(event)
+        {
+            $(this).addClass("kivalasztva").siblings().removeClass("kivalasztva");
+        }
+        );
+    }
+);
+
+$(document).ready(
+    function(){
+        $(".kicsit_nem").click(
+            function(event)
+        {
+            $(this).addClass("kivalasztva").siblings().removeClass("kivalasztva");
+        }
+        );
+    }
+);
+
+$(document).ready(
+    function(){
+        $(".semleges").click(
+            function(event)
+        {
+            $(this).addClass("kivalasztva").siblings().removeClass("kivalasztva");
+        }
+        );
+    }
+);
+
+$(document).ready(
+    function(){
+        $(".kicsit_igaz").click(
+            function(event)
+        {
+            $(this).addClass("kivalasztva").siblings().removeClass("kivalasztva");
+        }
+        );
+    }
+);
+
+$(document).ready(
+    function(){
+        $(".félig_igaz").click(
+            function(event)
+        {
+            $(this).addClass("kivalasztva").siblings().removeClass("kivalasztva");
+        }
+        );
+    }
+);
+
+$(document).ready(
+    function(){
+        $(".nagyon_igaz").click(
+            function(event)
+            {
+                $(this).addClass("kivalasztva").siblings().removeClass("kivalasztva");
+            }
+        );
+    }
+);
